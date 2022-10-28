f3b8edf3a2bb3ec7fd925723a0195b111ba33714
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2022-10-28 16:12:05 +0700

    Menambahkan Model,View, Controller Main sebagai pondasi kodingan awal

---
+++ TBPBO_Nathan_Juan_Michael_Evan_Yudis/src/Model/Pengguna.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author yudis
+ */
+public class Pengguna {
+    private String nama;
+    private String password;
+    //private KehadiranInterface kehadiran;
+
+    public Pengguna(String nama, String password) {
+        this.nama = nama;
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
