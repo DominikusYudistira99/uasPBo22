6a9db7357c35cf240d5d637352b0e9fdcd67aa00
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2022-11-18 13:08:58 +0700

    Menambahkan kodingan uas

---
+++ UasPbo22/src/Main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import java.util.ArrayList;
+import View.Tampilanutama;
+
+/**
+ *
+ * @author yudis
+ */
+public class Main {
+
+    public static void main(String[] args) {
+     Tampilanutama.tampilanutama();
+     
+    }
+}
---
+++ UasPbo22/src/View/Tampilanutama.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author yudis
+ */
+public class Tampilanutama {
+    public static void tampilanutama(){
+    JFrame frame = new JFrame("Main Menu");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JButton button = new JButton();
+        button.setText("Login Pengguna");
+        JButton button2 = new JButton();
+        button2.setText("Regristrasi Pengguna Baru");
+        JButton button3 = new JButton();
+        button3.setText("Lihat Data Pengguna Berdasarkan Kategori Dipilih");
+        
+        panel.add(button);
+        panel.add(button2);
+        panel.add(button3);
+      
+
+        frame.add(panel);
+        frame.setSize(200, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    
+    }
+}
